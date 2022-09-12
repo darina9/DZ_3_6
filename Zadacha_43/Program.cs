91f1570dc461d4cbd850cb7d194b57709140adbd
Darina <krastinadv@gmail.com>
2022-09-12 12:37:58 +0300

    Zadacha_43

---
+++ Zadacha_43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double numX = (b2 - b1)/(k1 - k2);
+double numY = k2 * numX + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: {numX},{numY}");
